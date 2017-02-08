22715d2a4c55dc99ef70acb3e196448b9bd9583a
Magcox5 <magcox5@yahoo.com>
2017-02-07 16:45:54 -0800

    Added getStudentLocations to PinTableViewController and ran it to make sure no errors occurred.

--- OnTheMap/PinTableViewController.swift
+++ OnTheMap/PinTableViewController.swift
@@ -14,7 +14,7 @@ class PinTableViewController: UITableViewController {
 
     // MARK:  Variables
     
-    let studentLocations = StudentArray.sharedInstance.thisStudentArray
+    var studentLocations = StudentArray.sharedInstance.thisStudentArray
 
     @IBOutlet var pinTableView: UITableView!
     
@@ -71,5 +71,47 @@ class PinTableViewController: UITableViewController {
 //        }
 //    }
     
+    private func getStudentLocations() {
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=100&order=-updatedAt")! as URL)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 5. Parse the data */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+            } catch {
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* 6. Parse the data for Result */
+            if let pinResults = parsedResult["results"] {
+                print(pinResults!)
+                // Store student locations in data structure
+                self.studentLocations = StudentArray.arrayFromResults(results: pinResults as! [[String : AnyObject]])
+            }
+            // Switch to Main Queue to display pins on map
+            DispatchQueue.main.async {
+ //               self.displayStudentLocations()
+            }
+        }
+        task.resume()
+        
+    }
     
 }
