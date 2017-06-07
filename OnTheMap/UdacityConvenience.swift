63f7e81e668fa289a4d92d5f511eeeb9a92b31df
Magcox5 <magcox5@yahoo.com>
2017-06-07 11:17:25 -0700

    Refactored UdacityConvenience methods to remove duplicate code...

--- OnTheMap/UdacityConvenience.swift
+++ OnTheMap/UdacityConvenience.swift
@@ -16,80 +16,100 @@ extension UdacityClient {
     func authenticateWithViewController(_ hostViewController: UIViewController, userName: String, password: String, completionHandlerForAuth: @escaping (_ success: Bool, _ errorString: String?) -> Void)
     {
         appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
         // chain completion handlers for each request so that they run one after the other
-        getSessionID(username: userName, password: password, completionHandlerForSessionID:
-        { (success, sessionID, errorString) in
-            
+        getUdacityResult(username: userName, password: password, completionHandlerForUdacityResult: { (success, parsedResult, errorString) in
             if success {
-                // success! we have the sessionID!
-                print(sessionID as Any)
-                self.appDelegate.sessionID = sessionID
-                        self.getFirstName(userID: self.appDelegate.udacityUserID, completionHandlerForUdacityName: { (success, firstName, errorString) in
-                            if success {
-                                print(firstName as String!)
-                                self.appDelegate.udacityFirstName = firstName!
-                                self.getLastName(userID: self.appDelegate.udacityUserID, completionHandlerForUdacityName: { (success, lastName, errorString) in
-                                    if success {
-                                        print(lastName as Any)
-                                        self.appDelegate.udacityLastName = lastName!
-                                        completionHandlerForAuth(true, nil)
-                                    } else {
-                                        completionHandlerForAuth(false, "Can't get last name")
-                                    }
-                                    
-                                })
-                                
-                    } else {
-                        completionHandlerForAuth(false, "Unable to complete login...")
-                    }
-                })
-            }
-            
+                self.getSessionID(parsedResult: parsedResult, completionHandlerForSessionID:
+                    { (success, sessionID, errorString) in
+                        if success {
+                        // success! we have the sessionID!
+//                            self.appDelegate.sessionID = sessionID
+                            self.getUserID(parsedResult: parsedResult!, completionHandlerForUserID: { (success, userID, errorString) in
+                                if success {
+                                    self.getUserDetails(userID: userID!, completionHandlerForUserDetails: { (success, userDetails, errorString) in
+                                        if success {
+                                            self.getFirstName(userDetails: userDetails , completionHandlerForUdacityName: { (success, firstName, errorString) in
+                                                if success {
+ //                                                   self.appDelegate.udacityFirstName = firstName!
+                                                    self.getLastName(userDetails: userDetails, completionHandlerForUdacityName: { (success, lastName, errorString) in
+                                                        if success {
+//                                                            self.appDelegate.udacityLastName = lastName!
+                                                            completionHandlerForAuth(true, nil)
+                                                        } else {
+                                                            completionHandlerForAuth(false, "Can't get last name")
+                                                        }
+                                                    })
+                                                } else {
+                                                    completionHandlerForAuth(false, "Can't get first name")
+                                                }
+                                            })
+                                        } else {
+                                            completionHandlerForAuth(false, "Can't get user details")
+                                        }
+                                    })
+                                }else {
+                                    completionHandlerForAuth(false, "Can't get user id")
+                                }
+                            })
+                        } else {
+                            completionHandlerForAuth(false, "Can't get session ID")
+                        }
+                    })
+                } else {
+                    completionHandlerForAuth(false, "Can't parse Udacity data as JSON")
+                }
         })
 
-    }
-    
+        }
 
-    func getSessionID(username: String, password: String, completionHandlerForSessionID: @escaping (_ success: Bool, _ sessionID: String?, _ errorString: String?) ->Void)
-    {
-        /* 1. Set the parameters */
-        let jsonBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}"
-        let parameters =
-        [UdacityClient.ParameterKeys.ApiKey: UdacityClient.Constants.ApiKey]
-        print(parameters)
-        /* 2/3. Build the URL, Configure the request */
-        var request = URLRequest(url: NSURL(string: UdacityClient.Constants.UdacityURL)! as URL)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = jsonBody.data(using: String.Encoding.utf8)
-        
-        /* 4. Make the request */
-        let task = self.appDelegate.session.dataTask(with: request, completionHandler: { (data, response, error) in
-            
-            
+    func getUdacityResult(username: String, password: String, completionHandlerForUdacityResult: @escaping (_ success: Bool, _ parsedResult: AnyObject?, _ errorString: String?) ->Void)
+        {
+            /* 1. Set the parameters */
+            let jsonBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}"
+//            let parameters = [UdacityClient.ParameterKeys.ApiKey: UdacityClient.Constants.ApiKey]
+            /* 2/3. Build the URL, Configure the request */
+            var request = URLRequest(url: NSURL(string: UdacityClient.Constants.UdacityURL)! as URL)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonBody.data(using: String.Encoding.utf8)
+                
+            /* 4. Make the request */
+            let task = self.appDelegate.session.dataTask(with: request, completionHandler: { (data, response,error) in
+                    
+                    
             /* GUARD: Was there an error? */
             guard (error == nil) else {
                 print(error as Any)
-                completionHandlerForSessionID(false, nil, "Login Failed (SessionID)")
+                completionHandlerForUdacityResult(false, nil, "Login Failed (SessionID)")
                 return
             }
-            
+                    
             // Strip off 1st 5 characters in data
             let newData = (data! as NSData).subdata(with: NSMakeRange(5, (data?.count)! - 5))
-            print(newData)
-            
-            
+                    
             /* 5. Parse the data */
             let parsedResult: AnyObject!
             do {
                 parsedResult = try JSONSerialization.jsonObject(with: newData, options: .allowFragments) as AnyObject!
             } catch {
-                completionHandlerForSessionID(false, nil, "Could not parse the data as JSON: '\(newData)'")
-                return
+                    completionHandlerForUdacityResult(false, nil, "Could not parse the data as JSON: '\(newData)'")
+                    return
             }
-            
+                    
+            /* 6. Use the data! */
+                    
+            /* 7. Start the request */
+            completionHandlerForUdacityResult(true, parsedResult, nil)
+                    
+            })
+            task.resume()
+                
+        }
+
+            func getSessionID(parsedResult: AnyObject!, completionHandlerForSessionID: @escaping (_ success: Bool, _ sessionID: String?, _ errorString: String?) ->Void)
+    {
+        
             
             /* GUARD: First, is there a session in our result? */
             guard let sessionData = parsedResult["session"] as? NSDictionary
@@ -105,71 +125,72 @@ extension UdacityClient {
                     return
             }
             
-            /* GUARD: Is there a userID in our result? */
-            if let userData = parsedResult["account"] as? [String: AnyObject] {
-                print(userData)
-                let userID = userData["key"]
-                print("My user id is:  \(userID!)")
-                self.appDelegate.udacityUserID = userID as! String
-            }
-            else {
-                completionHandlerForSessionID(false, nil, "Cannot find userID in \(parsedResult!)")
-                return
-            }
-            
+        
             /* 6. Use the data! */
-            print("Session Id is: \(sessionID)")
             self.appDelegate.sessionID = sessionID
 
             /* 7. Start the request */
             completionHandlerForSessionID(true, self.appDelegate.sessionID, nil)
-
-        })
-        task.resume()
-        
     }
 
-    func getUserID(sessionID: String, completionHandlerForUserID: @escaping (_ success: Bool, _ userID: String?, _ errorString: String?) ->Void)
+        func getUserID(parsedResult: AnyObject, completionHandlerForUserID: @escaping (_ success: Bool, _ userID: String?, _ errorString: String?) ->Void)
     {
-        
+        /* GUARD: Is there a userID in our result? */
+        if let userData = parsedResult["account"] as? [String: AnyObject] {
+            let userID = userData["key"]
+            self.appDelegate.udacityUserID = userID as! String
+            completionHandlerForUserID(true, userID as? String, nil)
+            return
+        }
+        else {
+            completionHandlerForUserID(false, nil, "Cannot find userID in \(parsedResult)")
+            return
+        }
     }
     
-    func getFirstName(userID: String, completionHandlerForUdacityName: @escaping (_ success: Bool, _ firstName: String?, _ errorString: String?) ->Void)
+    func getUserDetails(userID: String, completionHandlerForUserDetails: @escaping (_ success: Bool, _ userDetails: [String: AnyObject], _ errorString: String?) -> Void)
     {
         let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/users/\(userID)")!)
-//        let session = URLSession.shared
+
         let task = self.appDelegate.session.dataTask(with: request as URLRequest) { data, response, error in
             if error != nil { // Handle error...
-                completionHandlerForUdacityName(false, nil, (error as! String))
+                completionHandlerForUserDetails(false, ["":"" as AnyObject], (error as! String))
                 return
             }
             let range = Range(5..<data!.count)
             let newData = data?.subdata(in: range)
-            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
-            
             /* 5. Parse the data */
             let parsedResult: [String: AnyObject]
             do {
                 parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! [String: AnyObject]
             } catch {
-                completionHandlerForUdacityName(false, nil, "Could not parse udacity user data as JSON")
+                completionHandlerForUserDetails(false, ["":"" as AnyObject], "Could not parse udacity user data as JSON")
                 return
             }
             
             /* GUARD: Is there a user in our result? */
             guard let userData = parsedResult["user"] as? [String: AnyObject]
                 else {
-                    completionHandlerForUdacityName(false, nil, "Cannot find 'user' in \(parsedResult)")
+                    completionHandlerForUserDetails(false, ["":"" as AnyObject], "Cannot find 'user' in \(parsedResult)")
                     return
             }
-            print("Yes, we have a user!")
+            completionHandlerForUserDetails(true, userData, nil)
+            return
             
-            
+        }
+        /* 7. Start the request */
+        task.resume()
+        
+        
+    }
+
+    func getFirstName(userDetails: [String: AnyObject], completionHandlerForUdacityName: @escaping (_ success: Bool, _ firstName: String?, _ errorString: String?) ->Void)
+    {
+        
             /* GUARD: Is there a first name in our result? */
-            if let firstName = userData["first_name"] as? String {
-                print("My first name is:  \(firstName)")
+            if let firstName = userDetails["first_name"] as? String {
                 self.appDelegate.udacityFirstName = firstName
-                completionHandlerForUdacityName(true, firstName, "Found First Name")
+                completionHandlerForUdacityName(true, firstName, nil)
                 return
             }
             else {
@@ -177,48 +198,15 @@ extension UdacityClient {
                 return
             }
             
-        }
-        /* 7. Start the request */
-        task.resume()
         
     }
     
-    func getLastName(userID: String, completionHandlerForUdacityName: @escaping (_ success: Bool,  _ lastName: String?,_ errorString: String?) ->Void)
+    func getLastName(userDetails: [String: AnyObject], completionHandlerForUdacityName: @escaping (_ success: Bool,  _ lastName: String?,_ errorString: String?) ->Void)
     {
-        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/users/\(userID)")!)
-//        let session = URLSession.shared
-        let task = self.appDelegate.session.dataTask(with: request as URLRequest) { data, response, error in
-            if error != nil { // Handle error...
-                completionHandlerForUdacityName(false, nil, (error as! String))
-                return
-            }
-            let range = Range(5..<data!.count)
-            let newData = data?.subdata(in: range)
-            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
-            
-            /* 5. Parse the data */
-            let parsedResult: [String: AnyObject]
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! [String: AnyObject]
-            } catch {
-                completionHandlerForUdacityName(false, nil, "Could not parse udacity user data as JSON")
-                return
-            }
-            
-            /* GUARD: Is there a user in our result? */
-            guard let userData = parsedResult["user"] as? [String: AnyObject]
-                else {
-                    completionHandlerForUdacityName(false, nil, "Cannot find 'user' in \(parsedResult)")
-                    return
-            }
-            print("Yes, we have a user!")
-            
-            
-            /* GUARD: Is there a first name in our result? */
-            if let lastName = userData["last_name"] as? String {
-                print("My last name is:  \(lastName)")
+            /* GUARD: Is there a last name in our result? */
+            if let lastName = userDetails["last_name"] as? String {
                 self.appDelegate.udacityLastName = lastName
-                completionHandlerForUdacityName(true, lastName, "Found Last Name")
+                completionHandlerForUdacityName(true, lastName, nil)
                 return
             }
             else {
@@ -226,11 +214,6 @@ extension UdacityClient {
                 return
             }
             
-        }
-        /* 7. Start the request */
-        task.resume()
-        
-
     }
     
 }
