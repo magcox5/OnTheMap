e62358c12db74200163b7225d42720bda42648bf
Magcox5 <magcox5@yahoo.com>
2017-06-17 21:34:37 -0700

    UdacityClient:  changed the error message to error.localizedDescription for connection failure,

--- OnTheMap/UdacityClient.swift
+++ OnTheMap/UdacityClient.swift
@@ -32,7 +32,7 @@ class UdacityClient : NSObject {
             
             /* GUARD: Was there an error? */
             guard (error == nil) else {
-                completionHandlerForUdacityResult(false, nil, "Failed to connect to server")
+                completionHandlerForUdacityResult(false, nil, error?.localizedDescription)
                 return
             }
             
